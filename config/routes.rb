d88ff12e2c8d29c700a82574772e1a97a4fa8d00
Iggy <igoririanto@rocketmail.com>
2016-07-29 20:51:52 -0700

    Changed config adapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
   namespace :api, defaults: { format: :json } do
 
      resources :users do

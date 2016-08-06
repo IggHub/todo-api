41b5f1760af053f2e323f3884ffc41464d6d3fca
Iggy <igoririanto@rocketmail.com>
2016-08-06 13:26:42 -0700

    Created index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   get 'welcome/index'
-
   get 'welcome/about'
-
+  root 'welcome#index'
   namespace :api, defaults: { format: :json } do
 
      resources :users do

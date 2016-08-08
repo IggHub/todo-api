108df6687f68c25f4407af1cbae427cfc2caa481
Iggy <igoririanto@rocketmail.com>
2016-08-08 16:13:15 -0700

    add email to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729005247) do
+ActiveRecord::Schema.define(version: 20160808231044) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160729005247) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "password_digest"
+    t.string   "email"
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
